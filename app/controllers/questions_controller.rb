5504ea7b6e1d70656f8b3bcdfc7b58aab4ffee59
Hari Kalanj <hkalanj@gmail.com>
2015-03-07 03:02:00 +0100

    question form and partial design

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,17 +1,21 @@
 class QuestionsController < ApplicationController
-	before_filter :question_params, :only => :create
+	before_filter :get_category, only: [:create]
 
 	def create
-		@question = Question.new(question_params)
+		@question = @category.questions.build(question_params)
 		if @question.save
 			redirect_to root_path, note: "Pitanje uspesno postavljeno!"
 		else
-			redirect_to root_path
+			redirect_to root_path, alert: "GRESKA! Niste ispravno uneli pitanje."
 		end
 	end
 
 	private
 		def question_params
-			params.require(:question).permit(:title, :content)
+			params.require(:question).permit(:title, :content, :category_id)
+		end
+
+		def get_category
+			@category = Category.find(params[:question][:category_id])
 		end
 end
--- app/models/question.rb
+++ app/models/question.rb
@@ -2,6 +2,7 @@ class Question < ActiveRecord::Base
   belongs_to :category
   belongs_to :user
   default_scope -> { order(created_at: :desc) }
-  #validates :category_id, presence: true
-  validates :title, presence: true, length: { minimum: 3, maximum: 60 }
+  validates :category_id, presence: true
+  validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  validates :content, length: { maximum: 300 }
 end
