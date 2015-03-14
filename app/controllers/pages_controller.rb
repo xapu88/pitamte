28c396a8f1fcb17ebd2a1db354cf84a1c4322faa
Hari Kalanj <hkalanj@gmail.com>
2015-03-14 02:28:58 +0100

    my questions and answers pages

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,18 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!, only: [:my_questions, :my_answers]
+
   def home
   	@categories = Category.all
   	@questions = Question.all
     @question = Question.new
+  end
+
+  def my_questions
+    @questions = Question.where(user_id: current_user.id)
+  end
+
+  def my_answers
+    @answers = Answer.where(user_id: current_user.id)
   end
 
   def contact
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ devise_for :users, controllers: { sessions: "users/sessions", registrations: "us
   resources :answers, only: [:create, :destroy]
 
   root 'pages#home'
+  get 'my_questions' => 'pages#my_questions'
+  get 'my_answers' => 'pages#my_answers'
   get 'contact' => 'pages#contact'
   get 'about' => 'pages#about'
   get 'agreements' => 'pages#agreements'
