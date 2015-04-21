f88feea5f7fc332f35781fa58da446ccc49e820b
Hari Kalanj <hkalanj@gmail.com>
2015-04-21 22:30:43 +0200

    user model omniauthable fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,
-         :omniauthable, omniauth_providers: [:facebook]
+         :omniauthable
 
   has_many :questions
   has_many :answers
