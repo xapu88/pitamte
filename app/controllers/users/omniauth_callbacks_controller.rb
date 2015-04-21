c49d0e9b3bd312d207c74f74ae2b6eac466166fb
Hari Kalanj <hkalanj@gmail.com>
2015-04-21 22:41:27 +0200

    trying to fix omniauth callbacks

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,9 +2,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   # You should also create an action method in this controller like this:
   def facebook
-    @user = User.find_or_create_for_facebook(env["omniauth.auth"])
-    flash[:notice] = "Prijava putem Facebooka uspela!"
-    sign_in_and_redirect @user, :event => :authentication
+    auth = env["omniauth.auth"]
+    provider = auth["provider"]
+    uid = auth["uid"]
+    data = auth["info"]
+    credentials = auth["credentials"]
+    token = credentials["token"]
+
+    if !current_user.blank?
+      binding = User.find_binding(provider, uid)
+
+      if binding.present?
+        binding.refresh_token(token)
+      else
+        current_user.bind_service(provider, uid, token)
+      end
+
+      redirect_to setting_path, :notice => "Bind facebook account successfully."
+    else
+      @user = User.find_or_create_for_facebook(uid, data, token)
+      sign_in_and_redirect @user, :event => :authentication, :notice => "Signed in successfully."
+    end
   end
 
   # More info at:
