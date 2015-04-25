6a055d3a189591a09de15bbf6275bf613be9c5ca
Hari Kalanj <hkalanj@gmail.com>
2015-04-25 03:19:01 +0200

    admin login fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,7 +11,11 @@ class ApplicationController < ActionController::Base
 	end
 
 	def after_sign_in_path_for(resource)
-	  session[:previous_url] || root_path
+		if resource.class == AdminUser
+			return admin_root_path
+		else
+	  	session[:previous_url] || root_path
+	  end
 	end
 
   private
